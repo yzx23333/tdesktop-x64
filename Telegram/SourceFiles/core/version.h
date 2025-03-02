60c47797cc4b5d535074e15812f2616a6e5c2451
frknkrc44 <krc440002@gmail.com>
2025-03-02 23:57:03 +0300

    use correct upstream version number

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -26,4 +26,4 @@ constexpr auto AppVersion = 1001058;
 constexpr auto AppVersionStr = "1.1.59";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
-constexpr auto UpstreamVersion = "5.10.3";
+constexpr auto UpstreamVersion = "5.11.1";
